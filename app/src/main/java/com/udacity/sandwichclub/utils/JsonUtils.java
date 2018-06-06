8e6bb37a79890486f7307604da2404dfd55ad000
Devil2020 <Devil2020@yahoo.com>
2018-06-06 18:18:09 +0200

    Done2

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -19,13 +19,22 @@ public class JsonUtils {
         JSONObject ob2=ob1.getJSONObject("name");
         sandwich.setMainName(ob2.getString("mainName"));
         String data1= ob2.getString("alsoKnownAs");// Here Also Known
-       List<String > flist1= Arrays.asList(data1.split(","));
-        sandwich.setDescription(ob1.getString("description"));
+        if(data1.equals("[]")){
+            data1="Message not available";
+            List<String> flist1 = new ArrayList<>(1);
+            flist1.add(data1);
+            sandwich.setAlsoKnownAs(flist1);
+        }
+        else {
+            List<String> flist1 = Arrays.asList(data1.split(","));
+            sandwich.setAlsoKnownAs(flist1);
+        }
+       sandwich.setDescription(ob1.getString("description"));
         sandwich.setImage(ob1.getString("image"));
         sandwich.setPlaceOfOrigin(ob1.getString("placeOfOrigin"));
        String data2=ob1.getString("ingredients");
         List<String > flist2= Arrays.asList(data2.split(","));
-        sandwich.setAlsoKnownAs(flist1);
+       // sandwich.setAlsoKnownAs(flist1);
         sandwich.setIngredients(flist2);
         return sandwich;
     }
